45ac177ef25370b16b18a918c70aa692de5abb8a
Hector <hectorbiminchumo@gmail.com>
2024-08-28 00:33:30 -0500

    read, update and delete books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,6 @@
+const Book = require("./../models/Book")
+
 exports.create = async (req, res) => {
-    const Book = require("./../models/Book")
     
     // Del formulario, creamos variables y asignamos valores
     const { 
@@ -32,4 +33,96 @@ exports.create = async (req, res) => {
         })
     }
     
+}
+exports.readAll = async (req, res) => {
+    try {
+        const books = await Book.find({})
+
+        res.json({
+            msg: "Libros obtenidos con exito",
+            data: books
+        })
+
+    } catch (error) {
+
+        res.status(500).json({
+            msg: "Hubo un error obteniendo los datos",
+            error: error
+        })
+        
+    }
+}
+exports.readOne = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const book = await Book.findById(id)
+
+        res.json({
+            msg: "Libro obtenido con exito",
+            data: book
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            msg: "hubo un error obteniendo los datos.",
+            error: error
+        })
+    }
+}
+exports.edit = async (req, res) => {
+    
+    const { id } = req.params
+    const {
+        nombre,
+        precio,
+        color,
+        imagen,
+        descripcion
+    } = req.body
+
+    try {
+        const updatedBook = await Book.findByIdAndUpdate(
+            id,
+            {
+                nombre,
+                precio,
+                color,
+                imagen,
+                descripcion
+            },
+            {new: true}
+        )
+        res.json({
+            msg: "Libro actualizado con exito",
+            data: updatedBook
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "Hubo un error con la actualizacion del libro",
+            error: error
+        })
+    }
+
+}
+exports.delete = async (req, res) => {
+
+	const { id } = req.params
+    
+	try {
+        
+        const deletedBook = await Book.findByIdAndDelete({_id: id})
+
+		res.json({
+			msg: "Libro borrado con exito",
+			data: deletedBook
+		})
+
+	} catch (error) {
+		res.status(500).json({
+			msg: "Hubo un error borrando el libro",
+			error: error
+		})
+	}
+
 }
--- routes/books.js
+++ routes/books.js
@@ -6,6 +6,10 @@ const bookController = require("./../controllers/bookController")
 
 // 2. Ruteo
 router.post("/create", bookController.create)
+router.get("/readAll", bookController.readAll)
+router.get("/readone/:id", bookController.readOne)
+router.put("/edit/:id", bookController.edit)
+router.delete("/delete/:id", bookController.delete)
 
 
 // 3. Exportaciones
